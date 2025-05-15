7c8a29b9104051f95399390fc0912ed55571a525
Jean Franco Yauri Vargas <131926070+Jeanfr07@users.noreply.github.com>
2025-05-15 00:30:58 -0500

    Add files via upload

--- Aplicación Recibo de Pago Smart/conexion.py
+++ Aplicación Recibo de Pago Smart/conexion.py
@@ -1,16 +1,14 @@
 import mysql.connector
-from mysql.connector import Error
 
 def conectar():
-    try:
-        conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='root',
-            database='smartdb'
-        )
-        if conn.is_connected():
-            return conn
-    except Error as e:
-        print(f"Error al conectar a la base de datos: {e}")
-        return None
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        database='smartdb'
+    )
+    conn.autocommit = True
+    return conn
+
+def desconectar(conn):
+    conn.close()
